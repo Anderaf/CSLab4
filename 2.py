113d5f21db4b54ebe643de75d2f0812b710122f7
Anderaf <62837264+Anderaf@users.noreply.github.com>
2023-12-05 01:50:10 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,9 +1,13 @@
 import xmltodict
 import yaml
+import time
 
 input = open('schedule.xml', 'r', encoding='utf-8')
 output = open('scheduleFromLib.yaml', 'w', encoding='utf-8')
 
+start_time = time.perf_counter()
 xmlstring = input.read()
-xmldict = xmltodict.parse(xmlstring)
-yaml.dump(xmldict, output, encoding='UTF-8', allow_unicode=True)
+for i in range(100):
+    xmldict = xmltodict.parse(xmlstring)
+    yaml.dump(xmldict, output, encoding='UTF-8', allow_unicode=True)
+print(time.perf_counter() - start_time)
